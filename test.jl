2fd1c1b4aee23fad35310a898c5a05d903aa1217
Josek20 <alexshugaylo@gmail.com>
2020-11-26 14:54:06 +0100

    redefenition of base methods

--- test.jl
+++ test.jl
@@ -6,19 +6,24 @@ using StatsBase
 
 x_plot = []
 y_plot = []
-#cost function, new one
 #write down into LaTex SGD
-#Validation loss
-#Later 
 
 struct Layer
 	b::Vector
 	W::Matrix
 end
 
+
+
 struct Network
 	layers::Vector{Layer}
+	
+	#inner contructor
+	#Network{T}(ngenes::UInt) where T<:Num
 end
+
+
+
 
 function Layer(in::Int,out::Int)
 	W = randn(out,in)
@@ -54,16 +59,35 @@ function sigma_prime(x)
 	return sigma(x)*(1-sigma(x))
 end
 
+function Base.:*(x::Float64,layer::Layer)
+	map(layer) do
+	end
+end
+
+function Base.:*(x::Float64,model::Network)
+	map(model.layers) do(layer)
+		
+	end
+end
+
+function Base.:-(model::Network,delta_model::Network)
+	map(model.layers,delta_model.layers) do (m_l,d_l)
+		
+	end
+end
+
 
 function sgd(train_data,epochs,mini_batch_size,eta,model,test_data=0)
 	if test_data != 0
 		n_test = length(test_data)
 	end
 	for epoch=1:epochs
-		mini_batches = train_data[shuffle(1:end)]
-		#map(mini_batch->updating_mini_batch(random_batch(train_data,mini_batch_size),eta,model),1:mini_batch_size:length(train_data))
-		map(min_batch->updating_mini_batch(mini_batches[min_batch:min_batch+mini_batch_size-1],eta,model),1:mini_batch_size:length(train_data))
-		#test_data = test_data[shuffle(1:end)]
+		map(1:mini_batch_size:length(train_data)) do x
+			rb = random_batch(train_data,mini_batch_size)
+			updating_mini_batch(rb,eta,model)
+		end
+
+		# Should be simplified by 1 function
 		push!(x_plot,sum(map(x->sum(cost(model(x[1])[2][3],x[2]))/length(test_data),test_data)))
 		push!(y_plot,epoch)
 		if test_data != 0
@@ -72,14 +96,12 @@ function sgd(train_data,epochs,mini_batch_size,eta,model,test_data=0)
 		else
 			println("Epoch ",epoch,"complited")
 		end
-		#error()
 	end
 end
 
 
 function random_batch(x::Vector,batch_size::Int)
 	indx = sample(1:Base.size(x,1),batch_size,replace = false)
-	display(indx)
 	return x[indx]
 end
 
@@ -89,14 +111,16 @@ function updating_mini_batch(mini_batch,eta,model)
 	noble_b = map(x->zeros(size(x.b)),model.layers) 
 	noble_w = map(x->zeros(size(x.W)),model.layers)
 	for i=1:length(mini_batch)
-		delta_b,delta_w = backprop(mini_batch[i][1],mini_batch[i][2],model,length(model.layers)+1)
+		delta_b,delta_w = backprop(mini_batch[i][1],mini_batch[i][2],model)
 		all_W,all_b = map(l->l.W,model.layers),map(l->l.b,model.layers)
 
 		noble_w = noble_w+delta_w
 		noble_b = noble_b+delta_b
 	
-		new_W = map((_W,n_w)->_W-(eta/length(mini_batch)).*n_w,all_W,noble_w)
-		new_b = map((_b,n_b)->_b-(eta/length(mini_batch)).*n_b,all_b,noble_b)
+		#new_W = map((_W,n_w)->_W-(eta/length(mini_batch)).*n_w,all_W,noble_w)
+		#new_b = map((_b,n_b)->_b-(eta/length(mini_batch)).*n_b,all_b,noble_b)
+		new_W = all_W-(eta/length(mini_batch)).*noble_w
+		new_b = all_b-(eta/length(mini_batch)).*noble_b
 		for i=1:length(model.layers) 
 			model.layers[i].W .= new_W[i]
 			model.layers[i].b .= new_b[i]
@@ -105,8 +129,20 @@ function updating_mini_batch(mini_batch,eta,model)
 	end
 end
 
+function back(zs::Vector,activations::Vector,cost_::Vector,model::Network,delta_model::Network)
+	delta = 0
+	for i = length(model.layers):-1:1
+		#display(i)till 1
+		sp = sigma_prime.(zs[i])
+		delta = (i == length(model.layers) ? cost_ : model.layers[i+1].W'*delta).*sp
+		delta_model.layers[i].b .+= delta
+		delta_model.layers[i].W .+= delta*activations[i]'
+	end
+end
 
-function backprop(x::Vector,y::Int,model::Network,num_layers=3)
+
+
+function backprop(x::Vector,y::Int,model::Network)
 	noble_b = map(x->zeros(size(x.b)),model.layers) 
 	noble_w = map(x->zeros(size(x.W)),model.layers)
 	zs,activations = model(x) 
@@ -115,11 +151,12 @@ function backprop(x::Vector,y::Int,model::Network,num_layers=3)
 
 	noble_b[end] .= delta 		
 	noble_w[end] .= delta*activations[end-1]'
+
 	for i = length(model.layers)-1:-1:1
-		z = zs[i]
-		sp = sigma_prime.(z)
+		#z = zs[i]
+		sp = sigma_prime.(zs[i])
 		layer = model.layers[i+1]
-		W,b = layer.W,layer.b
+		W = layer.W
 		delta = (W'*delta).*sp
 		noble_b[i] .= delta
 		noble_w[i] .= delta*activations[i]'
@@ -141,14 +178,10 @@ function cost_derivative(output_active,y)
 end
 
 
-
-
 function evaluate(test_data,model)
 	test_result = map(x->(findmax(model(x[1])[2][3])[2]-1,x[2]),test_data)
-	#display(test_result)
 	#True = 1, False = 0
 	return sum(map(x->x[1]==x[2],test_result)) 
-	#cumsum(map(x->x[1]==x[2],test_result),dims=1)[end]
 end
 
 
@@ -160,30 +193,27 @@ function zipping(sample,label::Array)
 	return a
 end
 
-
+function graph!(x_data::Array,y_data::Array)
+	gr()
+	plot!(x_data,y_data,label="Evol of cost")
+	
+end
 #Initiating NN
 
 training_x,train_y = MNIST.traindata(Float32)
 test_x,test_y = MNIST.testdata(Float32)
 tr_x = MNIST.traintensor(Float64)
-println("<============================>")
-
-
 
 model = Network(
 	[Layer(784,30),
 	Layer(30,10)]
 )
+delta_model = Network(
+	[Layer(zeros(30),zeros(30,784)),
+	Layer(zeros(10),zeros(10,30))]
+)
 
 data = zipping(training_x,train_y)
 tst = zipping(test_x,test_y)
-display("====")
-sgd(data[1:1000],30,10,0.10,model,tst)
+#sgd(data,30,10,0.10,model,tst)
 
-#Ploting evol of cost 
-gr()
-plot(y_plot,x_plot,label="evol of cost")
-scatter!(y_plot,x_plot,label="evol of cost")
-xlabel!("Number of epoch")
-ylabel!("MSE")
-png("C:/Users/alexs/Julia/test\\plot")
