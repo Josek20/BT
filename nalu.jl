de2d219f9659817795a1b15010f108576e522c58
Josek20 <alexshugaylo@gmail.com>
2020-12-19 18:25:46 +0100

    New version

--- nalu.jl
+++ nalu.jl
@@ -1,49 +1,56 @@
+using Flux
 using Flux:@epochs
 using Flux.Data: DataLoader
 using Flux:gradient,params
 using Flux.Optimise: update!,Descent
-using MLDatasets
+using Distributions
+using LinearAlgebra
+using NeuralArithmetic
+using Plots
+using Random
 
-xtrain,ytrain = MLDatasets.MNIST.traindata(Float32)
-xtest,ytest = MLDatasets.MNIST.testdata(Float32)
+Random.seed!(0)
 
-xtrain = Flux.flatten(xtrain)
-xtest = Flux.flatten(xtest)
-eta = 3e-4
+#few functions: squer, some polynomial, logorithm 
+#and their values on the plot
+#how it works on different ranges, like shift the range,
+#or make it biger smaller. How does renges afferc generalization perfomance
 
-function onehot(y,sz)
-	tmp = zeros(sz)
-	tmp[y+1] = 1
-	return tmp
+beta = 1e-4
+lr = 1e-3
+batchsize = 100
+lrange = 0.01
+rrange = 2
+function task(x::Vector)
+	#a,b = x[1],x[2]
+	#[a+b,a-b,a*b,a/b]
+	[x.^2 + 2*x;sqrt.(x);log.(x);abs.(x)]
 end
 
-function accuracy(data,model)
-	acc = 0
-	for (x,y) in data
-		acc+=sum(Flux.onecold(cpu(model(x))) .== Flux.onecold(cpu(y)))*1/size(x,2)
-	end
-	acc/length(data)
+#task(x::Matrix) = mapslices(task,x,dims=1)
+
+function generate_data(bs::Int)
+	a = Uniform(lrange,rrange)
+	b = Uniform(0.01,rrange)
+	#x = Float32.(rand(Product([a,b]),bs))
+	x = Float32.(rand(Product([a]),bs))
+	y = task(x)
+	(x,y)
 end
 
-ytrain = Flux.onehotbatch(ytrain,0:9)
-ytest = Flux.onehotbatch(ytest,0:9)
+
+data = [generate_data(batchsize) for _ in 1:5000]
+
+nac_model = Chain(NALU(1,12),NALU(12,4))
+pc = Flux.params(nac_model)
+cost(x,y) = Flux.mse(nac_model(x),y)
+loss(x,y) = cost(x,y)
+opt = ADAM(lr)
+cb = Flux.throttle(()->(@info "training..." loss(data[1]...) cost(data[1]...)),1)
+Flux.train!(cost,pc,data,opt,cb=cb)
+tm = [data[1][2][1,:],nac_model(data[1][1])[1,:]]
+scatter(data[1][1][1:batchsize],tm)
+savefig("test_train.png")
 
 
-train_data = DataLoader(xtrain,ytrain,batchsize=1000,shuffle=true)
-test_data = DataLoader(xtest,ytest,batchsize=1000)
 
-#lossfunc(x) = sum(W*x+b);
-
-#grads = gradient(()->lossfunc(x),params([W,b]))
-
-
-model = Chain(Dense(784,32,relu),Dense(32,10))
-
-loss(x,y) = sum(Flux.logitcrossentropy(model(x),y))
-
-opt = ADAM(eta)
-@epochs 10 Flux.train!(loss,params(model),train_data,opt)
-
-@show accuracy(train_data,model)
-
-@show accuracy(test_data,model)
